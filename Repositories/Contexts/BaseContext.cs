f093239a477a683ceec5bf98be4309fe8edbfedc
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-10-04 11:12:42 +0300

    refactor: new base class (BaseContext).

---
+++ Repositories/Contexts/BaseContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebGallery.Repositories.Contexts
+{
+    public abstract class BaseContext : DbContext
+    {
+        private readonly ConfigurationBuilder? postgreConfigurationBuilder = new ConfigurationBuilder();
+
+        private readonly IConfigurationRoot? postgreConfigurationRoot;
+
+        private readonly string ?password;
+
+        private readonly string ?port;
+
+        private readonly string ?username;
+
+        private readonly string ?host;
+
+        public string? DatabaseName { get; init; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql($"Host={host};Port={port};Database={DatabaseName};Username={username};Password={password}");
+        }
+
+
+        public BaseContext()
+        {
+            postgreConfigurationBuilder.AddJsonFile("postgresConfig.json");
+
+            postgreConfigurationRoot = postgreConfigurationBuilder.Build();
+
+            password = postgreConfigurationRoot.GetConnectionString(nameof(password));
+            port = postgreConfigurationRoot.GetConnectionString(nameof(port));
+            username = postgreConfigurationRoot.GetConnectionString(nameof(username));
+            host = postgreConfigurationRoot.GetConnectionString(nameof(host));
+        }
+    }
+}
---
+++ Repositories/Contexts/StatisticsContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebGallery.Models.Statistics;
+
+namespace WebGallery.Repositories.Contexts
+{
+    public class StatisticsContext : BaseContext
+    {
+        public DbSet<Page> Pages { get; set; } = null!;
+
+        public DbSet<Visit> Visits { get; set; } = null!;
+    }
+}
