f93bcf7c22f1a27f509e0ae17a6b3ca407388192
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-11-06 18:31:37 +0300

    style: some cosmitic changes in the CommentController class.

--- WebGallery/src/WebGallery.Presenters/CommentController.cs
+++ WebGallery/src/WebGallery.Presenters/CommentController.cs
@@ -8,42 +8,57 @@ namespace WebGallery.Web
     public class CommentController : ControllerBase
     {
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] CreateCommentDto request, CancellationToken cancellationToken)
+        public async Task<IActionResult> Create(
+            [FromBody] CreateCommentDto request, 
+            CancellationToken cancellationToken)
         {
             return Ok("Create Comment");
         }
 
 
         [HttpGet]
-        public async Task<IActionResult> Get([FromQuery] GetCommentsByExhibitionDto request, CancellationToken cancellationToken)
+        public async Task<IActionResult> Get(
+            [FromQuery] GetCommentsByExhibitionDto request,
+            CancellationToken cancellationToken)
         {
             return Ok("Get all comments");
         }
 
 
         [HttpGet("{commentId:guid}")]
-        public async Task<IActionResult> GetById([FromRoute] Guid commentId, CancellationToken cancellationToken)
+        public async Task<IActionResult> GetById(
+            [FromRoute] Guid commentId,
+            CancellationToken cancellationToken
+            )
         {
             return Ok("Get comment by id");
         }
 
 
         [HttpPatch("{commentId:guid}")]
-        public async Task<IActionResult> Update([FromRoute] Guid commentId, UpdateCommentDto request, CancellationToken cancellationToken)
+        public async Task<IActionResult> Update(
+            [FromRoute] Guid commentId,
+            UpdateCommentDto request,
+            CancellationToken cancellationToken)
         {
             return Ok("Update comment");
         }
 
 
         [HttpPatch("{commentId:guid}/rate")]
-        public async Task<IActionResult>RateComment([FromRoute] Guid commentId, RateCommentDto request, CancellationToken cancellationToken)
+        public async Task<IActionResult>  RateComment(
+            [FromRoute] Guid commentId, 
+            RateCommentDto request,
+            CancellationToken cancellationToken)
         {
             return Ok("Rate comment");
         }
 
 
         [HttpDelete("{commentId:guid}")]
-        public async Task<IActionResult> Delete([FromRoute] Guid commentId, CancellationToken cancellationToken)
+        public async Task<IActionResult> Delete(
+            [FromRoute] Guid commentId,
+            CancellationToken cancellationToken)
         {
             return Ok("Delete comment");
         }
