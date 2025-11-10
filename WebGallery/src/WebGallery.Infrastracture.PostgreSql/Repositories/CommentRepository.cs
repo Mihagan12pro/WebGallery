463f33f7c7c329eca149e71e3bfa50bc73ce6d42
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-11-10 11:05:17 +0300

    feat: new project - WebGallery.Infrastracture.PostgreSql.

---
+++ WebGallery/src/WebGallery.Infrastracture.PostgreSql/Repositories/CommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using WebGallery.Application.Comments;
+using WebGallery.Domain.Comments;
+
+namespace WebGallery.Infrastracture.PostgreSql.Repositories;
+
+public class CommentRepository : ICommentsRepository
+{
+    public async Task<Guid> AddAsync(Comment comment, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public async Task<Guid> UpdateAsync(Comment comment, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public async Task<Comment> GetByIdAsync(Guid id, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public async Task<Guid> DeleteAsync(Comment comment, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public async Task<int> GetUserReputationAsync(Guid userId, CancellationToken cancellationToken) => throw new NotImplementedException();
+}
