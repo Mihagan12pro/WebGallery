a1819fa89dd1015aa2bacbed8239fab4cdb77ce9
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-12-28 22:53:31 +0300

    feat: add RolePermission and AuthorisationOptions classes

---
+++ WebGallery/src/WebGallery.Infrastracture.PostgreSql/Options/Authorization/AuthorizationOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace WebGallery.Infrastracture.PostgreSql.Options.Authorization
+{
+    public class AuthorizationOptions
+    {
+        public RolePermissions[] RolePermissions { get; set; } = [];
+    }
+}
---
+++ WebGallery/src/WebGallery.Infrastracture.PostgreSql/Options/Authorization/RolePermissions.cs
@@ -0,0 +1,9 @@
+﻿namespace WebGallery.Infrastracture.PostgreSql.Options.Authorization
+{
+    public class RolePermissions
+    {
+        public string Role { get; set; } = string.Empty;
+
+        public string[] Permissions { get; set; } = [];
+    }
+}
