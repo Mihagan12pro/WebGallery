7eaac043c9947f3b3cdb67ecfc56bd10a6c94fa3
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-12-28 23:29:12 +0300

    feat: modify RolePermissionConfiguration (add RolePermission[] ParseRolePermissions())

--- WebGallery/src/WebGallery.Infrastracture.PostgreSql/EntityConfigurations/Users/PivotTables/RolePermissionConfiguration.cs
+++ WebGallery/src/WebGallery.Infrastracture.PostgreSql/EntityConfigurations/Users/PivotTables/RolePermissionConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebGallery.Domain.Users.Permissions;
 using WebGallery.Domain.Users.PivotTables;
+using WebGallery.Domain.Users.Roles;
 using WebGallery.Infrastracture.PostgreSql.Options.Authorization;
 
 namespace WebGallery.Infrastracture.PostgreSql.EntityConfigurations.Users.PivotTables
@@ -13,6 +15,21 @@ namespace WebGallery.Infrastracture.PostgreSql.EntityConfigurations.Users.PivotT
         public void Configure(EntityTypeBuilder<RolePermission> builder)
         {
             builder.HasKey(r => new { r.RoleId, r.PermissionId });
+
+            builder.HasData(ParseRolePermissions());
+        }
+
+        private RolePermission[] ParseRolePermissions()
+        {
+            return _authorizationOptions.RolePermissions.
+                SelectMany(rp => rp.Permissions.
+                    Select(p => new RolePermission
+                    {
+                       RoleId = (int)Enum.Parse<Enums.Role>(rp.Role),
+
+                       PermissionId = (int)Enum.Parse<Enums.Permission>(p),
+                    }))
+                    .ToArray(); 
         }
 
         public RolePermissionConfiguration(AuthorizationOptions authorizationOptions)
